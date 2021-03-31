8893cef77b6776043e7d28364a15b3d22b9db0fa
Barbara Law <barbara.rm.law@gmail.com>
2021-03-30 17:37:55 -0700

    styling updates

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ async function getYokaiInfo() {
     const data = await response.json()
 
     console.log(data)
+    document.querySelector('.directions').style.display = 'none';
+    document.querySelector('.results').style.display = 'block';
     document.getElementById('yokaiPicThumb').src = data.yokaiPicThumb
     document.getElementById('yokaiPicThumb').alt = `${data.name} thumbnail picture`
     document.getElementById('yokaiPic').href = data.yokaiPic
@@ -16,8 +18,10 @@ async function getYokaiInfo() {
     document.getElementById('medalPicThumb').src = data.medalPicThumb
     document.getElementById('medalPicThumb').alt = `${data.name} medal`
     document.getElementById('medalPic').href = data.medalPic
-    document.getElementById('tribe').innerText = data.tribe
-    document.querySelector('p').innerText = data.medalliumBio
+    document.getElementById('tribe').innerText = 'Tribe:   ' + data.tribe
+    document.querySelector('#bio').innerText = data.medalliumBio
+    document.querySelector('input').innerText = ''
+    document.querySelector('input').placeholder = 'Enter another Yokai name'
 
   } catch(err) {
     console.log(err)
